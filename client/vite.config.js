750cceedc2d95b51c2ae64a5cbef998421a11d5c
Sebolelo-Mohale <lavaemohale@gmail.com>
2025-09-12 22:08:41 +0200

    Fix commit after cleaning node_modules

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: '/wings-cafe/',
   server: {
     proxy: {
       "/api": "http://localhost:4000",
